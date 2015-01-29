4f1aaee0ebcda03d9cad6e61ee9b1672827ee0d0
David Dawson <david.dawson@gmail.com>
2015-01-29 08:16:29 +0000

    Revert "Add a combination of optimitistic and pesimistic locking to prevent dupliscate requests causing random behaviour for the order checkout process"
    This reverts commit 75ec585006cac58efbd9c60df3b507c2a37dd295.

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -80,7 +80,6 @@ module Spree
 
     before_create :create_token
     before_create :link_by_email
-    before_create :set_version_uuid
     before_update :homogenize_line_item_currencies, if: :currency_changed?
 
     validates :email, presence: true, if: :require_email
@@ -638,22 +637,10 @@ module Spree
         payments.offset_payment.exists? # how old versions of spree stored refunds
     end
 
-    def update_version_uuid
-      update_column(:version_uuid, secure_random_uuid)
-    end
-
     private
 
     def link_by_email
       self.email = user.email if self.user
-    end
-
-    def set_version_uuid
-      self.version_uuid = secure_random_uuid
-    end
-
-    def secure_random_uuid
-      SecureRandom.uuid
     end
 
     # Determine if email is required (we don't want validation errors before we hit the checkout)
